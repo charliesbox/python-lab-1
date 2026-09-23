e1f852eb7dab92805ff1f6d72102c74b060c7fa4
charlie <maks.tuzhin@mail.ru>
2026-09-24 01:38:48 +0300

    Add toolkit directory, add tokenizer with int/float support

---
+++ src/toolkit/tokenizer.py
@@ -0,0 +1,59 @@
+def isoperator(char: str) -> bool:
+    return char in ['+', '-', '*', '/', '//', '%', '**']
+
+
+def read_number(s: str, i: int) -> tuple[tuple[str, int | float], int]:
+    number = ''
+    fraction = False
+
+    while True:
+        if i >= len(s):
+            break
+
+        elif s[i].isdigit():
+            number += s[i]
+
+        elif s[i] == '.' and not fraction:
+            number += s[i]
+            fraction = True
+
+        elif s[i] == '.' and fraction:
+            raise ValueError('Invalind element in the expression.')
+
+        else:
+            break
+
+        i += 1
+
+    if fraction:
+        return (('NUMBER', float(number)), i)
+    else:
+        return (('NUMBER', int(number)), i)
+
+
+def read_operator(s: str, i: int) -> tuple[tuple[str, str], int]:
+    return (('OPERATOR', s), i)
+
+def tokenize(s: str) -> list[tuple[str, int | float | str]]:
+    tokens: list[tuple[str, int | float | str]] = []
+    token: tuple[str, int | float | str]
+    i = 0
+    while i < len(s):
+        if s[i].isdigit() or s[i] == '.':
+            token, i = read_number(s, i)
+            tokens.append(token)
+            continue
+
+        elif isoperator(s[i]):
+            token, i = read_operator(s[i], i)
+            tokens.append(token)
+            i += 1
+            continue
+
+        elif s[i] == ' ':
+            i += 1
+            continue
+
+        else:
+            raise ValueError('Invalid symbol in the expression.')
+    return tokens
